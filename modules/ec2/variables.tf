2a0690de830b16779886f8dc6ff026eeab7f09fa
Mohammed <i23281@verisk.com>
2024-10-27 13:26:58 -0400

    Roboshop-Terraform

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,6 +2,6 @@ variable "environment" {}
 variable "instance_type" {}
 variable "component_name" {}
 variable "app_port" {}
-variable "vpc_id" {}
+# variable "vpc_id" {}
 variable "zone_id" {}
 variable "domain_name" {}
