203e57a17ad70243a39055ba42f5ed2b1c9306a6
Mohammed <i23281@verisk.com>
2024-12-12 14:47:20 -0500

    Terraform - eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,12 +1,14 @@
 resource "aws_eks_cluster" "main" {
   name = "${var.env}-eks"
 
-  # access_config {
-  #   authentication_mode = "API"
-  # }
+
 
   role_arn = aws_iam_role.eks-cluster-role.arn
   # version  = "1.31"
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
 
   access_config {
     authentication_mode                         = "API_AND_CONFIG_MAP"
