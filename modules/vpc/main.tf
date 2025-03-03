fe7c568497dfeaaaaa4f22989d34c9feca3a39d4
Mohammed <i23281@verisk.com>
2025-03-02 22:55:57 -0500

    Network VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.54.1"
-    }
-  }
-}
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = {
